c819f441b50d21d6ce0624e77e07d9c40e1adf45
Travis Hunsberger <tfhunsberger@gmail.com>
2022-12-29 17:06:32 -0500

    useEffect and useCallback examples added

--- src/components/TripList/TripList.js
+++ src/components/TripList/TripList.js
@@ -1,18 +1,34 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import './TripList.scss';
 
 function TripList() {
 
     //init of state for using data
     const [trips, setTrips] = useState([]);
+    const [url, setUrl] = useState('http://localhost:3000/trips')
+
+    {/* When using functions inside of useEffect, each time the page or component renders, useEffect will reference the function and notice differences and keep refiring and refiring 
+    causeing an endless loop. To get around that, you need to use useCallback hook. The hook creates a cached version of a function. On every evaluation of the component,
+    the cached function is not being recreated. Therefore, not being seen as changed by useEffect, and wont refire.
+
+    The useCallback hook also has a dependency array as a second argument. This depend. array will tell the callback hook when to create a new function */}
+    const fetchTrips = useCallback(async () => {
+        const response = await fetch(url);
+        const json = await response.json();
+        setTrips(json);
+    }, [url])
 
     //if you want the function to only run once inside useEffect, you must and always need to pass in an empty array as a second arg (a dependency)
     useEffect(() => {
-        fetch('http://localhost:3000/trips')
-            .then(response => response.json())
-            //when we have json data back, we want to call setTrips/hook function to update state
-            .then(json => setTrips(json));
-    } , [])
+        // //we cant use async functions inside of useEffect. We can however, extract the fetch logic into it's own asyn function and run it inside useEffect
+        // fetch(url)
+        //     .then(response => response.json())
+        //     //when we have json data back, we want to call setTrips/hook function to update state
+        //     .then(json => setTrips(json));
+        fetchTrips();
+    } , [fetchTrips])
+
+    console.log(trips);
 
     return (
         <div className="trip-list">
@@ -25,6 +41,10 @@ function TripList() {
                     </li>
                 ))}
             </ul>
+            <div className="trip-list__filtered">
+                <button className="trip-list__button" onClick={() => setUrl('http://localhost:3000/trips?loc=Europe')}>European Trips</button>
+                <button className="trip-list__button" onClick={() => setUrl('http://localhost:3000/trips')}>ALl Trips</button>
+            </div>
         </div>
     )
 }
